37077920ac7ba00a2431835c943d4440a45e356f
Kirill Nepomiluev <nepomiluev.kirill@gmail.com>
2021-06-18 21:23:35 +0300

    андроид подключение к firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,8 @@
+import 'dart:async';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:iherb_helper/themes/custom_theme.dart';
 import 'package:iherb_helper/themes/themes.dart';
@@ -5,13 +10,31 @@ import 'package:iherb_helper/widgets/app_scaffold.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-SharedPreferences prefs;
+String versoinApp = "1.0.0 от 18.06.2021";
 
+SharedPreferences prefs;
+Map<String, dynamic> curUser;
+FirebaseFirestore store = FirebaseFirestore.instance;
+User currentUser = FirebaseAuth.instance.currentUser;
+Map<String, Object> userData;
+Map<String, Object> homeData;
+bool statusStartWork = false;
 bool isDarkTheme;
+bool test = false;
+bool showDialogShiftOpen = false;
+double distanceToHome = 0;
+double minRange = 100;
+double minRangeToOrder = 100;
+
+const String mapRoute = '/map';
+DateTime dateTime;
+Timer globalTimer;
+TextEditingController taskPageTextController = TextEditingController(text: "ok");
+
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  // await Firebase.initializeApp();
+   await Firebase.initializeApp();
   await SharedPreferences.getInstance().then((value) {
     prefs = value;
     if (prefs != null && prefs.get("isDarkTeme") != null) {
@@ -35,9 +58,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      theme: CustomTheme.of(context),
       home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
